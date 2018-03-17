99962df5255508fa59f75c2b2e62e7949a95f6ca
joshuajcarson <joshuajcarson@gmail.com>
2018-03-17 09:33:50 -0500

    fixed card draw behavior

--- control_ai/game_manager.py
+++ control_ai/game_manager.py
@@ -7,12 +7,17 @@ class GameManager(object):
     def __init__(self):
         self.deck = pd.read_csv('./data/control_deck.csv')
 
-    def get_random_index_of_deck(self):
-        return random.randint(0, len(self.deck.query("location == 'deck'").index))
+    def get_random_index_of_deck(self, max_int):
+        return random.randint(0, max_int-1)
 
-    def draw_five_cards_for_player(self, player='one'):
-        self.deck.iloc[self.get_random_index_of_deck(), 3] = 'player_' + str(player)
-        self.deck.iloc[self.get_random_index_of_deck(), 3] = 'player_' + str(player)
-        self.deck.iloc[self.get_random_index_of_deck(), 3] = 'player_' + str(player)
-        self.deck.iloc[self.get_random_index_of_deck(), 3] = 'player_' + str(player)
-        self.deck.iloc[self.get_random_index_of_deck(), 3] = 'player_' + str(player)
+    def draw_starting_hand(self, player='one'):
+        for x in range(0, 5):
+            self.draw_card_for_player(player)
+
+    def draw_card_for_player(self, player='one'):
+        cards_in_deck_location = self.deck.index[self.deck['location'] == 'deck'].tolist()
+        card_to_draw_index = self.get_random_index_of_deck(len(cards_in_deck_location))
+        self.deck.at[cards_in_deck_location[card_to_draw_index], 'location'] = 'player_' + str(player)
+
+    def card_could_be_drawn(self):
+        return len(self.deck.query("location == 'deck'").index) > 0
--- test/test_game_manager.py
+++ test/test_game_manager.py
@@ -14,21 +14,33 @@ class TestGameManager(unittest.TestCase):
     def test_game_manager_deck_is_all_located_in_deck_on_creation(self):
         self.assertEqual(52, len(self.class_under_test.deck.query("location == 'deck'")))
 
-    def test_game_manager_deck_called_for_random_int_when_making_hand_for_player_one(self):
-        indexes_for_player_one = [1, 3, 7, 23, 10]
-        self.class_under_test.get_random_index_of_deck = unittest.mock.MagicMock(side_effect=indexes_for_player_one)
-        self.class_under_test.draw_five_cards_for_player('one')
-        player_one_cards = self.class_under_test.deck.iloc[indexes_for_player_one].query("location == 'player_one'")
-        self.assertEqual(5, len(player_one_cards.index))
-        self.assertEqual(5, self.class_under_test.get_random_index_of_deck.call_count)
+    def test_game_manager_draw_starting_hand_calls_draw_card_five_times(self):
+        player = 'one'
+        self.class_under_test.draw_card_for_player = unittest.mock.MagicMock()
+        self.class_under_test.draw_starting_hand(player)
+        self.assertEqual(5, self.class_under_test.draw_card_for_player.call_count)
 
-    def test_game_manager_deck_called_for_random_int_when_making_hand_for_player_two(self):
-        indexes_for_player_two = [1, 3, 7, 23, 10]
-        self.class_under_test.get_random_index_of_deck = unittest.mock.MagicMock(side_effect=indexes_for_player_two)
-        self.class_under_test.draw_five_cards_for_player('two')
-        player_two_cards = self.class_under_test.deck.iloc[indexes_for_player_two].query("location == 'player_two'")
-        self.assertEqual(5, len(player_two_cards.index))
-        self.assertEqual(5, self.class_under_test.get_random_index_of_deck.call_count)
+    def test_game_manager_draw_starting_hand_calls_draw_card_with_same_arguments(self):
+        player = 'two'
+        self.class_under_test.draw_card_for_player = unittest.mock.MagicMock()
+        self.class_under_test.draw_starting_hand(player)
+        self.class_under_test.draw_card_for_player.assert_called_with(player)
+
+    def test_draw_card_for_player_one_draws_a_card_from_deck(self):
+        self.class_under_test.get_random_index_of_deck = unittest.mock.MagicMock()
+        self.class_under_test.get_random_index_of_deck.return_value = 1
+        self.class_under_test.draw_card_for_player('one')
+        self.assertEqual(1, len(self.class_under_test.deck.query("location == 'player_one'")))
+        self.assertEqual(51, len(self.class_under_test.deck.query("location == 'deck'")))
+        self.assertEqual(1, self.class_under_test.get_random_index_of_deck.call_count)
+
+    def test_card_could_be_drawn_returns_true_if_any_card_still_in_deck(self):
+        self.assertEqual(True, self.class_under_test.card_could_be_drawn())
+
+    def test_card_could_be_drawn_returns_false_if_no_card_still_in_deck(self):
+        for x in range(0, 52):
+            self.class_under_test.draw_card_for_player('one')
+        self.assertEqual(False, self.class_under_test.card_could_be_drawn())
 
     def test_game_manager_deck_has_four_rift_cards(self):
         cards = self.class_under_test.deck.query("name == 'rift'")
